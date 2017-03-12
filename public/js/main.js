a0c91b88d7e621c8218ca47b7b4baea37030b718
OlegSavytskyi <oleg.savytsky@gmail.com>
2017-03-12 10:49:41 +0200

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,131 @@
+(function() {
+	 $(".hide").hide();
+	 $(".getjson").on('click', function(){
+		delOldRepos();
+		var link = "https://api.github.com/users/";
+		var userName = $(".getcod").val();
+		$.getJSON(link + userName + '/repos', function(data, status, xhr){
+			$(".hide").show();
+			data.forEach (
+				function (val){
+					$(".getrepos").append("<li class='listRepos' data-toggle='tooltip' data-title='open issues:"+val.open_issues+"'>"+val.name+"</li>");
+				}
+			 );
+			handleTooltip();
+			handleIssueClick(userName);
+		});
+	});	
+	
+	/**
+	 * Show issues of the chosen project(repository).
+	 * @param {string} userName.
+	 */
+	function handleIssueClick (userName){
+		$(".getrepos").on('click', 'li', userName, function(e){
+			$(".blockIssue").remove();
+			var $this = $(this);
+			var $elementRepos=$this.text();
+			$(".nojs").prepend("<div class='blockIssue'><p>" + userName + " > " + $elementRepos + " > issue: " + "</p><div title='close' class='blockIssueClose'>x</div><ul class='issue'></ul></div>")
+			var linkIssue = "https://api.github.com/repos/";
+			$.getJSON(linkIssue + userName + '/' + $elementRepos +'/issues', function(data, status, xhr){
+				data.forEach (
+					function (val1){
+						$(".issue").append("<li>"+val1.title+"</li>");
+					}
+				)
+			});
+			blockIssueClose();
+		});
+	}
+
+	/**
+	 * Show tooltip on the mouse over event for the chosen repository.
+	 * @param {string} userName.
+	 */
+	function handleTooltip (){
+		$(".getrepos").on('mouseover', 'li' , (function(){
+			$('[data-toggle="tooltip"]').tooltip();
+		}));
+	}
+
+	/**
+	 * Dellete old repos of the chosen project(repository).
+	 */
+	function delOldRepos (){
+		var $oldRepos = $(".getrepos>li");
+		if ($oldRepos.length !== 0){
+			$oldRepos.remove();
+		}
+	}
+
+	/**
+	 * Close block Issue.
+	 */
+	function blockIssueClose (){
+		$(".blockIssueClose").on('click', function(){
+			$(".blockIssue").remove();
+		});	
+	};
+})();
+
+	
+/* 
+	
+var auth = btoa('OlegSavytskyi:Test123');
+jQuery.ajax({
+	type        : 'GET', 
+	url         : 'https://api.github.com/authorizations', 
+	headers: { 
+		"Authorization": "Basic " + auth 
+	},
+	success : function(data) {
+		console.log(data);
+		alert("Done!");
+	}
+	
+});
+
+	
+var auth = btoa('OlegSavytskyi:Test123');
+jQuery.ajax({
+	type: 'POST', 
+	url: 'https://api.github.com/repos/OlegSavytskyi/github-api/issues/1/comments', 
+	data: 	'{"body": "Hello!"}',
+	headers: { 
+		"Authorization": "Basic " + auth 
+	},
+	success : function(data) {
+		console.log(data);
+		alert("Done!");
+	}
+	
+});
+
+
+
+
+
+
+	
+jQuery.ajax ({ 
+	url: "https://api.github.com/authorizations", 
+	data: {"body": "Me too"},
+	beforeSend: function(xhr) { xhr.setRequestHeader("Authorization", "Basic " + btoa('OlegSavytskyi:Gjknfdf23')) },
+	succes: function(val){ alert(val)}
+});
+
+	
+	var auth = btoa('test:test');
+$.ajax({
+    type: 'GET',
+    url: 'http://example.com',
+    headers: {
+        "Authorization": "Basic " + auth
+    },
+    success : function(data) {
+    },
+	
+
+});
+	
+	 */
