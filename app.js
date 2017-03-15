3aa7a2128c06995d668378594ca666dcb906ca71
OlegSavytskyi <oleg.savytsky@gmail.com>
2017-03-15 18:49:28 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
-//var githubAuth = require( "../../lib/oath" );
-
+var githubAuth = require('connect-oauth-github');
 
 var port = process.env.PORT || 5000;
 
@@ -12,55 +11,46 @@ var callbackUrl = "https://github-api-v2.herokuapp.com/auth";
 
 var app = express();
 
-/* 
+// Initialize the Express application 
+// The application must have sessions enabled 
 app.use( express.cookieParser() );
 app.use( express.cookieSession({
-	secret: "your secret goes here"
+    secret: clientSecret
 }));
-
-// Initialize the GitHub OAuth client
+ 
+// Initialize the GitHub OAuth client 
 var gha = githubAuth.createClient({
-	id: clientId,
-	secret: clientSecret
+    id: clientId,
+    secret: clientSecret
 });
-
-// Add the route for the GitHub authorization callback
-// The path must match authorization callback URL for the GitHub application
-app.get( callbackUrl, gha.handshake );
-
-// Create a standard route which doesn't require any authorization
-app.get( "/", function( request, response ) {
-	response.send( "Welcome! Would you like to view a page with " +
-		"<a href='/required'>required authorization</a> or " +
-		"<a href='/optional'>optional authorization</a>?" );
-});
-
-// Create a route which requires authorization
+ 
+// Add the route for the GitHub authorization callback 
+// The path must match authorization callback URL for the GitHub application 
+app.get( "/auth", gha.handshake );
+ 
+// Create a route which requires authorization 
 app.get( "/required", gha.authorize, function( request, response ) {
-	var accessToken = gha.users[ request.sessionID ].accessToken;
-	response.send( "Your access token is " + accessToken );
+    var accessToken = gha.users[ request.sessionID ].accessToken;
+    response.send( "Your access token is " + accessToken );
 });
-
-// Create a route with optional authorization
+ 
+// Create a route with optional authorization 
 app.get( "/optional", function( request, response ) {
-	gha.isAuthorized( request, function( error, isAuthorized ) {
-		if ( error ) {
-			response.send( 500 );
-		}
-
-		var name = isAuthorized ?
-			gha.users[ request.sessionID ].accessToken :
-			"anonymous";
-
-		response.send( "Hello, " + name );
-	});
+    gha.isAuthorized( request, function( error, isAuthorized ) {
+        if ( error ) {
+            response.send( 500 );
+        }
+ 
+        var name = isAuthorized ?
+            gha.users[ request.sessionID ].accessToken :
+            "anonymous";
+ 
+        response.send( "Hello, " + name );
+    });
 });
-
-// Start listening for requests
-//app.listen( port );
-
-
-  */
+ 
+// Start listening for requests 
+//app.listen( 5000 );
  
 app.configure(function() {
  app.use('/', express.static(__dirname + '/public/'));
