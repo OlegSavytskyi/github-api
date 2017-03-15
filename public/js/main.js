b35320fe3be0d47ffed4e81d44e2346a5c1af709
OlegSavytskyi <oleg.savytsky@gmail.com>
2017-03-15 18:57:26 +0200

    add sing in github

--- public/js/main.js
+++ public/js/main.js
@@ -29,7 +29,7 @@
 	function signIn (){
 		$(".signin").on('click', function(){
 			alert('Sorry, this function is under development!');
-			/* $.get('https://github.com/login/oauth/authorize?client_id=6debf385832bd698f1d1',
+			$.get('https://github.com/login/oauth/authorize?client_id=6debf385832bd698f1d1',
 				function(data){
 				if (data.success) {
 					console.log('all ok');
@@ -37,7 +37,7 @@
 					console.log('error');
 				}
 			}, "json");
-			return false; */
+			return false;
 		});	
 	};
 	
