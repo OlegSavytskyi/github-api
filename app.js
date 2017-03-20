40d10c48671acb128ac3cad6fe92e42c31653e71
OlegSavytskyi <oleg.savytsky@gmail.com>
2017-03-20 14:20:21 +0200

    add sing in github

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ app.use(express.cookieParser());
  
 githubOAuth.on('token', function(token, serverResponse) {
   console.log('here is your shiny new github oauth token', token)
-  //serverResponse.cookie('connect.sess', token);
+  serverResponse.cookie('connect.sess', token.access_token);
   //var cookie = token.cookies.cookieName;
   //serverResponse.cookie('user_session', JSON.stringify(token));
   //serverResponse.redirect('https://github-api-v2.herokuapp.com/');
-  serverResponse.send(token.access_token);
+  //serverResponse.send(token.access_token);
 })
 
 
